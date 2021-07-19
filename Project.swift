95b6d97258e71d595eac013f441302c22fa8d0a1
kimxwan0319 <kimxwan0319@naver.com>
2021-07-20 02:28:13 +0900

    ⚙️ :: SwiftLint + Tuist 세팅

--- Project.swift
+++ Project.swift
@@ -18,7 +18,9 @@ let project = Project(
             sources: ["Sources/**"],
             resources: ["Resources/**"],
             actions: [
-                
+                TargetAction.pre(script: "${PODS_ROOT}/SwiftLint/swiftlint",
+                                 name: "SwiftLint"
+                )
             ],
             dependencies: [
                 .cocoapods(path: ".")

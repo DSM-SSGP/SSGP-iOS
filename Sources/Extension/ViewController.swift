5bbccbf8575f4d82949fe7e3c7f5d900a0b37784
kimxwan0319 <kimxwan0319@naver.com>
2021-07-26 19:50:52 +0900

    📑 :: ViewController Extension 파일 생성

---
+++ Sources/Extension/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  SSGP
+//
+//  Created by 김수완 on 2021/07/26.
+//  Copyright © 2021 com.ssgp. All rights reserved.
+//
+
+import Foundation
+
+extension ViewModel {
+    
+}

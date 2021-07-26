dc205d201a5d8a34bfe5d05ec018a4efd7225e9f
kimxwan0319 <kimxwan0319@naver.com>
2021-07-26 22:14:24 +0900

    ⚡️ :: TabBar 기본 세팅 및 관련 ViewController 생성

---
+++ Sources/View/MyPage/MyPageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MyPageViewController.swift
+//  SSGP
+//
+//  Created by 김수완 on 2021/07/26.
+//  Copyright © 2021 com.ssgp. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Sources/View/TabBarController.swift
+++ Sources/View/TabBarController.swift
@@ -7,15 +7,40 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class TabBarController: UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCustomTabBar()
         delegate = self
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+
+        let mapViewController = UINavigationController().then {
+            let rootViewController = MapViewController()
+            $0.setViewControllers([rootViewController], animated: true)
+            $0.title = "지도"
+            $0.tabBarItem = UITabBarItem(title: nil, image: R.image.tabBarMapIcon(), selectedImage: nil)
+        }
+        let productListViewController = UINavigationController().then {
+            let rootViewController = ProductListViewController()
+            $0.setViewControllers([rootViewController], animated: true)
+            $0.title = "제품"
+            $0.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "list.bullet"), selectedImage: nil)
+        }
+        let myPageViewController = UINavigationController().then {
+            let rootViewController = MyPageViewController()
+            $0.setViewControllers([rootViewController], animated: true)
+            $0.title = "마이페이지"
+            $0.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "person.fill"), selectedImage: nil)
+        }
+
+        let controllers = [mapViewController, productListViewController, myPageViewController]
+        self.viewControllers = controllers
     }
 
     // Delegate methods
@@ -24,5 +49,8 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         shouldSelect viewController: UIViewController)
     -> Bool {
         return true
+    }
+
+    func setCustomTabBar() {
     }
 }

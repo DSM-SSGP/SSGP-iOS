f81039ccba0770fae1f2fd33d6c2688d4cf3867f
jangseoyoung <jangseoyoung0402@gmail.com>
2021-07-28 01:48:34 +0900

    🔀 :: (#1) 드롭다운 버튼 세팅 완료

--- Sources/Extension/UIButton.swift
+++ Sources/Extension/UIButton.swift
@@ -11,22 +11,29 @@ import UIKit
 import DropDown
 
 extension UIButton {
-    
-    func sortList(button: UIButton) {
-        
+    func setDropDownButton() {
+        self.contentHorizontalAlignment = UIControl.ContentHorizontalAlignment.right
+        self.contentMode = .right
+        self.setTitle("인기순 ▾", for: .normal)
+        self.setTitleColor(R.color.accentColor(), for: .normal)
+        self.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        self.backgroundColor = .clear
+    }
+    func sortList() {
         let dropDown = DropDown()
-        
-        dropDown.backgroundColor = UIColor.
-        dropDown.cornerRadius = 3.0
-        dropDown.layer.borderWidth = 0.5
-        dropDown.layer.borderColor = UIColor.init(named: "DropDownStroke") as! CGColor
-        
+        dropDown.backgroundColor = R.color.background()
+        dropDown.textColor = R.color.accentColor()!
+        dropDown.selectedTextColor = R.color.accentColor()!
+        dropDown.selectionBackgroundColor = R.color.dropDownStroke()!
+        dropDown.cornerRadius = 3
+        dropDown.shadowOpacity = 0.3
         dropDown.dataSource = ["인기순", "추천", "최저가순"]
         dropDown.show()
-        dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
-            button.setTitle("\(item) ▼", for: .normal)
+        dropDown.selectionAction = { (_: Int, item: String) in
+            self.setTitle("\(item) ▾", for: .normal)
         }
-        
-        dropDown.anchorView = button
+        dropDown.anchorView = self
+        dropDown.bottomOffset = CGPoint(x: 0, y: (dropDown.anchorView?.plainView.bounds.height) ?? 00)
+        DropDown.appearance().textFont = UIFont.systemFont(ofSize: 15)
     }
 }

deceb6a3999021c562a498895d8c685a37eb1cbe
jangseoyoung <jangseoyoung0402@gmail.com>
2021-07-28 16:28:57 +0900

    ⚡️ :: DropDown 코드 구조 변경

--- Sources/Extension/UIButton.swift
+++ Sources/Extension/UIButton.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import DropDown
+import Then
 
 extension UIButton {
     func setDropDownButton() {
@@ -18,22 +19,24 @@ extension UIButton {
         self.setTitleColor(R.color.accentColor(), for: .normal)
         self.titleLabel?.font = UIFont.systemFont(ofSize: 15)
         self.backgroundColor = .clear
-    }
-    func sortList() {
-        let dropDown = DropDown()
-        dropDown.backgroundColor = R.color.background()
-        dropDown.textColor = R.color.accentColor()!
-        dropDown.selectedTextColor = R.color.accentColor()!
-        dropDown.selectionBackgroundColor = R.color.dropDownStroke()!
-        dropDown.cornerRadius = 3
-        dropDown.shadowOpacity = 0.3
-        dropDown.dataSource = ["인기순", "추천", "최저가순"]
-        dropDown.show()
-        dropDown.selectionAction = { (_: Int, item: String) in
-            self.setTitle("\(item) ▾", for: .normal)
+        func sortList() {
+            _ = DropDown().then {
+                $0.backgroundColor = R.color.background()
+                $0.textColor = R.color.accentColor()!
+                $0.selectedTextColor = R.color.accentColor()!
+                $0.selectionBackgroundColor = R.color.dropDownStroke()!
+                $0.cornerRadius = 3
+                $0.shadowOpacity = 0.3
+                $0.dataSource = ["인기순", "추천", "최저가순"]
+                $0.show()
+                $0.selectionAction = { (_: Int, item: String) in
+                    self.setTitle("\(item) ▾", for: .normal)
+                }
+                $0.anchorView = self
+                $0.bottomOffset = CGPoint(x: 0, y: ($0.anchorView?.plainView.bounds.height) ?? 00)
+            }
+            DropDown.appearance().textFont = UIFont.systemFont(ofSize: 15)
         }
-        dropDown.anchorView = self
-        dropDown.bottomOffset = CGPoint(x: 0, y: (dropDown.anchorView?.plainView.bounds.height) ?? 00)
-        DropDown.appearance().textFont = UIFont.systemFont(ofSize: 15)
     }
+    
 }

cdb260603123f0a4193a93ce61874e7ac22c0949
kimxwan0319 <kimxwan0319@naver.com>
2021-07-31 03:52:10 +0900

    ⚡️ :: 지도의 기본적인 UI 세팅

--- Sources/View/Map/MapViewController.swift
+++ Sources/View/Map/MapViewController.swift
@@ -7,24 +7,68 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import RxSwift
+import RxCocoa
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
 
+    // MARK: - Properties
+    private let disposeBag = DisposeBag()
+    private lazy var locationManager = CLLocationManager().then {
+        $0.delegate = self
+        $0.desiredAccuracy = kCLLocationAccuracyBest
+        $0.requestWhenInUseAuthorization()
+        $0.startUpdatingLocation()
+    }
+    private let mapView = MKMapView().then {
+        $0.layoutMargins = UIEdgeInsets(top: 0, left: 0, bottom: 15, right: 0)
+        $0.showsUserLocation = true
+        $0.setUserTrackingMode(.follow, animated: true)
+    }
+    private let noticeButton = UIBarButtonItem().then {
+        $0.image = UIImage(systemName: "bell.fill")
+        $0.tintColor = R.color.notSelectedIcon()
+    }
+
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+        setNavigationBar()
+        setupSubview()
+        moveMyLocation()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+
+    // MARK: - private method
+    private func setupSubview() {
+        self.view.addSubview(mapView)
+
+        mapView.snp.makeConstraints {
+            $0.left.right.top.equalToSuperview()
+            $0.bottom.equalToSuperview().offset(15)
+        }
+    }
+
+    private func setNavigationBar() {
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationItem.rightBarButtonItem = noticeButton
+    }
+
+    private func moveMyLocation() {
+        locationManager.userLocation().subscribe(onSuccess: { [weak self] in
+            let viewRegion = MKCoordinateRegion(
+                center: $0,
+                latitudinalMeters: 700,
+                longitudinalMeters: 700
+            )
+            self?.mapView.setRegion(viewRegion, animated: true)
+        })
+        .disposed(by: disposeBag)
+    }
 
 }

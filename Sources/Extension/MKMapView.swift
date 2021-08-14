c9eccb90cedf2aa95a295cdbd757e45f854e2f08
kimxwan0319 <kimxwan0319@naver.com>
2021-08-15 01:18:15 +0900

    ⚡️ :: 가장 가까이 있는 Annotation 찾아 주는 함수 `questNearestAnnotation()` 개발

--- Sources/Extension/MKMapView.swift
+++ Sources/Extension/MKMapView.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2021 com.ssgp. All rights reserved.
 //
 
-import Foundation
+import MapKit
+import RxSwift
+
+extension MKMapView {
+    /// 가장 가까운 Annotation 탐색
+    func questNearestAnnotation() -> MKAnnotation? {
+        var pins = self.annotations
+        if let currentLocation = self.userLocation.location {
+            for (index, pin) in pins.enumerated() where pin is MKUserLocation {
+                pins.remove(at: index)
+            }
+            let nearestPin: MKAnnotation? = pins.reduce((CLLocationDistanceMax, nil)) { (nearest, pin) in
+                let coord = pin.coordinate
+                let loc = CLLocation(latitude: coord.latitude, longitude: coord.longitude)
+                let distance = currentLocation.distance(from: loc)
+                return distance < nearest.0 ? (distance, pin) : nearest
+            }.1
+            return nearestPin
+        }
+        return nil
+    }
+}

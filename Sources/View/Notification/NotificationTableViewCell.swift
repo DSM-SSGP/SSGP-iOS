d3cf4e498b2042b2f1b1d406bde12307a63cb0ab
kimxwan0319 <kimxwan0319@naver.com>
2021-08-17 16:56:54 +0900

    📑 :: NotificationTableViewCell 파일 생성

---
+++ Sources/View/Notification/NotificationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationTableViewCell.swift
+//  SSGP
+//
+//  Created by 김수완 on 2021/08/17.
+//  Copyright © 2021 com.ssgp. All rights reserved.
+//
+
+import UIKit
+
+class NotificationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

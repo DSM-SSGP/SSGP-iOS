85855b82bdd1d26cc05fe99c1a7ea377438b6dd0
jangseoyoung <jangseoyoung0402@gmail.com>
2021-11-03 10:34:48 +0900

    ⚡️ :: UITextFieldExtension Underline메소드 삭제

--- Sources/Extension/UITextField.swift
+++ Sources/Extension/UITextField.swift
@@ -9,13 +9,4 @@
 import UIKit
 
 extension UITextField {
-
-    func underLine() {
-        let border = CALayer()
-        border.frame = CGRect(x: 0, y: self.frame.size.height-1, width: self.frame.width, height: 2)
-        border.borderWidth = 2
-        border.borderColor = R.color.accentColor()?.cgColor
-        self.layer.addSublayer(border)
-        self.adjustsFontSizeToFitWidth = true
-    }
 }

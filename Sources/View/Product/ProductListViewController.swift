1e6d83e5d3db75a4bda13f97bbbcedc9c732ee7a
jangseoyoung <jangseoyoung0402@gmail.com>
2021-11-18 14:42:19 +0900

    ⚡️ :: Tabman

--- Sources/View/Product/ProductListViewController.swift
+++ Sources/View/Product/ProductListViewController.swift
@@ -39,6 +39,8 @@ class ProductListViewController: TabmanViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationBar()
+        setViewControllers()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -48,5 +50,75 @@ class ProductListViewController: TabmanViewController {
     private func setNavigationBar() {
         setLargeTitleNavigationBar(title: "제품")
         self.navigationItem.rightBarButtonItem = barButtonItem
+        let searchController = UISearchController(searchResultsController: nil)
+        self.navigationItem.searchController = searchController
+    }
+
+    private func setViewControllers() {
+        let storeMainVC = StoreMainViewController()
+        let GS25VC = GS25ViewController()
+        let CUVC = CUViewController()
+        let miniStopVC = MiniStopViewController()
+        let sevenElevenVC = SevenElevenViewController()
+        let emartVC = EmartViewController()
+
+        [storeMainVC, GS25VC, CUVC, miniStopVC, sevenElevenVC, emartVC].forEach({ viewControllers.append($0) })
+
+        self.dataSource = self
+
+        let bar = TMBar.ButtonBar()
+        bar.layout.transitionStyle = .snap
+        bar.layout.alignment = .centerDistributed
+        bar.layout.contentMode = .fit
+        bar.backgroundView.style = .clear
+        bar.indicator.overscrollBehavior = .bounce
+        addBar(bar, dataSource: self, at: .top)
+    }
+}
+
+extension ProductListViewController: PageboyViewControllerDataSource, TMBarDataSource {
+
+    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+        viewControllers.count
+    }
+
+    func viewController(for pageboyViewController: PageboyViewController, at index: PageboyViewController.PageIndex) -> UIViewController? {
+        return viewControllers[index]
+    }
+
+    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+        return nil
+    }
+
+    func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
+        switch index {
+        case 0:
+            let item = TMBarItem(title: "")
+            item.title = "전체"
+            return item
+        case 1:
+            let item = TMBarItem(title: "")
+            item.title = "GS25"
+            return item
+        case 2:
+            let item = TMBarItem(title: "")
+            item.title = "CU"
+            return item
+        case 3:
+            let item = TMBarItem(title: "")
+            item.title = "MiniStop"
+            return item
+        case 4:
+            let item = TMBarItem(title: "")
+            item.title = "7"
+            return item
+        case 5:
+            let item = TMBarItem(title: "")
+            item.title = "Emart"
+            return item
+        default:
+            let item = TMBarItem(title: "")
+            return item
+        }
     }
 }
---
+++ Sources/View/Product/StoreViewControllers/StoreMainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StoreMainViewController.swift
+//  SSGP
+//
+//  Created by 장서영 on 2021/11/18.
+//  Copyright © 2021 com.ssgp. All rights reserved.
+//
+
+import UIKit
+
+class StoreMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

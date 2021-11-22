60f61a037f838b9884bc5da94e8dd8767cab275b
jangseoyoung <jangseoyoung0402@gmail.com>
2021-11-22 09:40:13 +0900

    ⚡️ :: ProductList Response Model 수정

--- Sources/Model/Networking/ProductList/ProductList.swift
+++ Sources/Model/Networking/ProductList/ProductList.swift
@@ -12,14 +12,18 @@ struct ProductResponse: Codable {
     var productId = Int()
     var name = String()
     var price = String()
-    var store_kind = String()
+    var store_kind = [String]()
     var image_path = String()
 
-    init(productId: Int, name: String, price: String, store_kind: String, image_path: String) {
+    init(productId: Int, name: String, price: String, store_kind: [String], image_path: String) {
         self.productId = productId
         self.name = name
         self.price = price
         self.store_kind = store_kind
         self.image_path = image_path
     }
+}
+
+struct ProductList: Codable {
+    var products = [ProductResponse]()
 }

a82238ffd3dd97af694ead0d78c9971d300ce72f
jangseoyoung <jangseoyoung0402@gmail.com>
2021-11-30 10:26:49 +0900

    ⚡️ :: ProductListViewModel Input Driver로 변경

--- Sources/ViewModel/Product/ProductListViewModel.swift
+++ Sources/ViewModel/Product/ProductListViewModel.swift
@@ -11,41 +11,33 @@ import RxSwift
 import RxCocoa
 
 class ProductListViewModel: ViewModel {
+
     let disposeBag = DisposeBag()
-    let output = Output()
+    var output = Output()
 
     struct Input {
-        let getPopularLists: Signal<Void>
-        let getRecommendLists: Signal<Void>
-        let getLowPriceLists: Signal<Void>
-        let likeButtonIsTapped: Signal<Void>
-        let productPostIsTapped: Signal<String>
+        let getPopularLists: Driver<Void>
+        let getRecommendLists: Driver<Void>
+        let getLowPriceLists: Driver<Void>
+        let likeButtonIsTapped: Driver<Void>
+        let productPostIsTapped: Driver<String>
     }
 
     struct Output {
-        let getLists: Driver<[ProductList]>
-        let detailID: Driver<String>
-        let result: Signal<String>
+        var getLists = PublishRelay<[ProductList]>()
+        var detailID = PublishRelay<String>()
+        var result = PublishRelay<String>()
     }
 
     func transform(_ input: Input) -> Output {
-        let getList = PublishRelay<[ProductList]>()
-        let getDetailRow = PublishSubject<String>()
-        let result = PublishSubject<String>()
-
-        input.getPopularLists.asObservable().subscribe(onNext: { listData in
-            HTTPClient.shared.networking(api: .popularityList, model: [ProductList])
-                .subscribe(onNext: { data in
-                    switch data {
-                    case .success:
-                        getList.accept(data)
-                    case .failure:
-                        result.onNext("로드 실패")
-                    }
-                }).disposeBag(by: self.disposeBag)
-        }).disposeBag(by: disposeBag)
-        return Output(getLists: getList.asDriver(onErrorJustReturn: []),
-                      detailID: getDetailRow.asDriver(onErrorJustReturn:""),
-                      result: result.asSignal(onErrorJustReturn: ""))
+        input.getPopularLists.asObservable().subscribe(onNext: {
+            HTTPClient.shared.networking(api: .popularityList,
+                                         model: [ProductList].self)
+                .subscribe(onSuccess: { _ in
+                    self.output.getLists
+                })
+                .disposed(by: self.disposeBag)
+        }).disposed(by: disposeBag)
+        return output
     }
 }

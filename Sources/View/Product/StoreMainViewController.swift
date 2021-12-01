2760e78826efffd344085678deacac939fbd4c9e
jangseoyoung <jangseoyoung0402@gmail.com>
2021-12-01 15:57:06 +0900

    ⚡️ :: GetLists Networking 끝

--- Sources/View/Product/StoreMainViewController.swift
+++ Sources/View/Product/StoreMainViewController.swift
@@ -18,7 +18,7 @@ class StoreMainViewController: UIViewController {
 
     let productModel = [ProductResponse]()
 
-    let viewModel = ProductListViewModel()
+    var getLists = PublishSubject<[ProductResponse]>()
 
     let tableView = UITableView().then {
         $0.register(ProductTableViewCell.self, forCellReuseIdentifier: "productCell")
@@ -41,91 +41,24 @@ class StoreMainViewController: UIViewController {
 
     init(index: Int) {
         super.init(nibName: nil, bundle: nil)
-        switch index {
-        case 0:
-            viewModel.output.getLists.asObservable()
-                .bind(
-                    to: tableView.rx.items(
-                        cellIdentifier: "ProductTableViewCell",
-                        cellType: ProductTableViewCell.self)) { _, element, cell in
-                            print("asdfasdfasdf")
-                            cell.bind(
-                                title: element.name,
-                                price: element.price,
-                                likeCount: element.like_count,
-                                store: element.brands)
-            }.disposed(by: disposeBag)
-        case 1:
-            viewModel.output.gs25Lists.asObservable()
-                .bind(
-                    to: tableView.rx.items(
-                        cellIdentifier: "ProductTableViewCell",
-                        cellType: ProductTableViewCell.self)) { _, element, cell in
-                            cell.bind(
-                                title: element.name,
-                                price: element.price,
-                                likeCount: element.like_count,
-                                store: element.brands)
-            }.disposed(by: disposeBag)
-
-        case 2:
-            viewModel.output.cuLists.asObservable()
-                .bind(
-                    to: tableView.rx.items(
-                        cellIdentifier: "ProductTableViewCell",
-                        cellType: ProductTableViewCell.self)) { _, element, cell in
-                            cell.bind(
-                                title: element.name,
-                                price: element.price,
-                                likeCount: element.like_count,
-                                store: element.brands)
-            }.disposed(by: disposeBag)
-
-        case 3:
-            viewModel.output.miniStopLists.asObservable()
-                .bind(
-                    to: tableView.rx.items(
-                        cellIdentifier: "ProductTableViewCell",
-                        cellType: ProductTableViewCell.self)) { _, element, cell in
-                            cell.bind(
-                                title: element.name,
-                                price: element.price,
-                                likeCount: element.like_count,
-                                store: element.brands)
-            }.disposed(by: disposeBag)
-
-        case 4:
-            viewModel.output.sevenElevenLists.asObservable()
-                .bind(
-                    to: tableView.rx.items(
-                        cellIdentifier: "ProductTableViewCell",
-                        cellType: ProductTableViewCell.self)) { _, element, cell in
-                            cell.bind(
-                                title: element.name,
-                                price: element.price,
-                                likeCount: element.like_count,
-                                store: element.brands)
-            }.disposed(by: disposeBag)
-
-        case 5:
-            viewModel.output.emart24Lists.asObservable()
-                .bind(
-                    to: tableView.rx.items(
-                        cellIdentifier: "ProductTableViewCell",
-                        cellType: ProductTableViewCell.self)) { _, element, cell in
-                            cell.bind(
-                                title: element.name,
-                                price: element.price,
-                                likeCount: element.like_count,
-                                store: element.brands)
-            }.disposed(by: disposeBag)
-
-        default:
-            break
-        }
+        getList()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    private func getList() {
+        getLists.asObservable()
+            .bind(
+                to: tableView.rx.items(
+                    cellIdentifier: "productCell",
+                    cellType: ProductTableViewCell.self)) { _, element, cell in
+                        cell.bind(
+                            title: element.name,
+                            price: element.price,
+                            likeCount: element.like_count,
+                            store: element.brands)
+        }.disposed(by: disposeBag)
     }
 }

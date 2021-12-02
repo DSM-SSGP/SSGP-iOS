a1a6f4dfd546f60c265666b1d9feea5732d1b987
jangseoyoung <jangseoyoung0402@gmail.com>
2021-12-03 03:01:07 +0900

    ⚡️ :: ProductDetailViewModel 수정

--- Sources/ViewModel/Product/ProductListViewModel.swift
+++ Sources/ViewModel/Product/ProductListViewModel.swift
@@ -21,20 +21,23 @@ class ProductListViewModel: ViewModel {
         let getRecommendLists: Driver<Void>
         let getLowPriceLists: Driver<Void>
        // let likeButtonIsTapped: Driver<Void>
-       // let productPostIsTapped: Driver<String>
+
         let searchProductIsTapped: Driver<String>
+
+        let itemSelected: Driver<(String, IndexPath)>
     }
 
     struct Output {
-        var getLists = PublishSubject<[ProductResponse]>()
-        var gs25Lists = PublishSubject<[ProductResponse]>()
-        var cuLists = PublishSubject<[ProductResponse]>()
-        var miniStopLists = PublishSubject<[ProductResponse]>()
-        var sevenElevenLists = PublishSubject<[ProductResponse]>()
-        var emart24Lists = PublishSubject<[ProductResponse]>()
+        var allList = BehaviorSubject<[ProductResponse]>(value: [])
+        var gs25Lists = BehaviorSubject<[ProductResponse]>(value: [])
+        var cuLists = BehaviorSubject<[ProductResponse]>(value: [])
+        var miniStopLists = BehaviorSubject<[ProductResponse]>(value: [])
+        var sevenElevenLists = BehaviorSubject<[ProductResponse]>(value: [])
+        var emart24Lists = BehaviorSubject<[ProductResponse]>(value: [])
 
-        var productDetail = PublishSubject<[ProductResponse]>()
         var likeresult = PublishSubject<Bool>()
+
+        var selectedItem = PublishSubject<ProductResponse>()
     }
 
     func transform(_ input: Input) -> Output {
@@ -49,18 +52,6 @@ class ProductListViewModel: ViewModel {
                     print(error)
                 }).disposed(by: self.disposeBag)
             }).disposed(by: disposeBag)
-
-//        input.productPostIsTapped.asObservable()
-//            .subscribe(onNext: { id in
-//                HTTPClient.shared.networking(
-//                    api: .productDetail(id),
-//                    model: [ProductResponse].self
-//                ).subscribe(onSuccess: { response in
-//                    self.output.productDetail.onNext(response)
-//                }, onFailure: { error in
-//                    print(error)
-//                }).disposed(by: self.disposeBag)
-//            }).disposed(by: disposeBag)
 
         input.getPopularLists.asObservable().subscribe(onNext: {
             HTTPClient.shared.networking(
@@ -95,6 +86,43 @@ class ProductListViewModel: ViewModel {
             }).disposed(by: self.disposeBag)
         }).disposed(by: disposeBag)
 
+        input.itemSelected.asObservable().subscribe(onNext: { type, indexPath in
+            switch type {
+            case "ALL":
+                do {
+                    let value = try self.output.allList.value()
+                    self.output.selectedItem.onNext(value[indexPath.row])
+                } catch { break }
+            case "CU":
+                do {
+                    let value = try self.output.cuLists.value()
+                    self.output.selectedItem.onNext(value[indexPath.row])
+                } catch { break }
+            case "GS25":
+                do {
+                    let value = try self.output.gs25Lists.value()
+                    self.output.selectedItem.onNext(value[indexPath.row])
+                } catch { break }
+            case "MINISTOP":
+                do {
+                    let value = try self.output.miniStopLists.value()
+                    self.output.selectedItem.onNext(value[indexPath.row])
+                } catch { break }
+            case "SEVENELEVEN":
+                do {
+                    let value = try self.output.sevenElevenLists.value()
+                    self.output.selectedItem.onNext(value[indexPath.row])
+                } catch { break }
+            case "EMART24":
+                do {
+                    let value = try self.output.emart24Lists.value()
+                    self.output.selectedItem.onNext(value[indexPath.row])
+                } catch { break }
+            default:
+                break
+            }
+        }).disposed(by: disposeBag)
+
         return output
     }
 
@@ -106,7 +134,7 @@ class ProductListViewModel: ViewModel {
         var emart24List = [ProductResponse]()
 
         model.forEach { item in
-            item.brands.forEach {
+            item.brands?.forEach {
                 switch $0 {
                 case "GS25":
                     gs25List.append(item)
@@ -129,6 +157,6 @@ class ProductListViewModel: ViewModel {
         output.miniStopLists.onNext(miniStopList)
         output.sevenElevenLists.onNext(sevenElevenList)
         output.emart24Lists.onNext(emart24List)
-        output.getLists.onNext(model)
+        output.allList.onNext(model)
     }
 }
